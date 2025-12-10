4c69f76fbb065288b496226f0ff2f225a0c21b34
Ben Straubinger <bstraubi@au1.ibm.com>
2025-12-10 12:15:59 +1000

    removed unneeded mapping rules

--- mapping_rules/mmfa_post_result_success.js
+++
@@ -1,8 +0,0 @@
-// Copyright contributors to the IBM Verify Identity Access AAC Mapping Rules project.
-
-importClass(Packages.com.tivoli.am.fim.trustserver.sts.utilities.IDMappingExtUtils);
-
-IDMappingExtUtils.traceString("Entry MMFA success");
-
-success.setValue(true);
-
